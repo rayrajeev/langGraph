25515138d5b6fc263eb2ec8fd79407cd033feae6
rayrajeev <37581270+rayrajeev@users.noreply.github.com>
2025-10-12 13:06:36 -0500

    Update graph.py

--- src/agent/graph.py
+++ src/agent/graph.py
@@ -1,41 +1,54 @@
-from langchain_openai import ChatOpenAI
-from langgraph.graph import END, START, MessagesState, StateGraph
-from langgraph.prebuilt import ToolNode, tools_condition
+import random
+from typing import Literal, TypedDict
+from langgraph.graph import StateGraph, START, END
 
 
-# Tool
-def multiply(a: int, b: int) -> int:
-    """Multiplies a and b.
-
-    Args:
-        a: first int
-        b: second int
-    """
-    return a * b
+# State
+class State(TypedDict):
+    graph_state: str  # Required (TypedDict fields are required by default unless total=False or NotRequired is used)
 
 
-# LLM with bound tool
-llm = ChatOpenAI(model="gpt-4o")
-llm_with_tools = llm.bind_tools([multiply])
+# Conditional edge
+def decide_mood(state) -> Literal["node_2", "node_3"]:
+
+    # Often, we will use state to decide on the next node to visit
+    user_input = state["graph_state"]
+
+    # Here, let's just do a 50 / 50 split between nodes 2, 3
+    if random.random() < 0.5:
+
+        # 50% of the time, we return Node 2
+        return "node_2"
+
+    # 50% of the time, we return Node 3
+    return "node_3"
 
 
-# Node
-def tool_calling_llm(state: MessagesState):
-    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+# Nodes
+def node_1(state):
+    print("---Node 1---")
+    return {"graph_state": state["graph_state"] + " I am"}
+
+
+def node_2(state):
+    print("---Node 2---")
+    return {"graph_state": state["graph_state"] + " happy!"}
+
+
+def node_3(state):
+    print("---Node 3---")
+    return {"graph_state": state["graph_state"] + " sad!"}
 
 
 # Build graph
-builder = StateGraph(MessagesState)
-builder.add_node("tool_calling_llm", tool_calling_llm)
-builder.add_node("tools", ToolNode([multiply]))
-builder.add_edge(START, "tool_calling_llm")
-builder.add_conditional_edges(
-    "tool_calling_llm",
-    # If the latest message (result) from assistant is a tool call -> tools_condition routes to tools
-    # If the latest message (result) from assistant is a not a tool call -> tools_condition routes to END
-    tools_condition,
-)
-builder.add_edge("tools", END)
+builder = StateGraph(State)
+builder.add_node("node_1", node_1)
+builder.add_node("node_2", node_2)
+builder.add_node("node_3", node_3)
+builder.add_edge(START, "node_1")
+builder.add_conditional_edges("node_1", decide_mood)
+builder.add_edge("node_2", END)
+builder.add_edge("node_3", END)
 
 # Compile graph
-graph = builder.compile()
+graph = builder.compile()  # Compile graph
